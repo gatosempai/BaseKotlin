63f7f679494a32d1171687fea35ce147fae77aca
oruizp <oruizp@live.com>
2019-12-10 16:45:46 -0600

    Room feature task detail activity first approach added

--- app/src/main/java/dev/oruizp/basekotlin/feature/room/view/TaskDetailActivity.kt
+++ app/src/main/java/dev/oruizp/basekotlin/feature/room/view/TaskDetailActivity.kt
@@ -6,6 +6,15 @@ import dev.oruizp.basekotlin.R
 
 class TaskDetailActivity : AppCompatActivity() {
 
+    // Extra for the task ID to be received in the intent
+    val EXTRA_TASK_ID = "extraTaskId"
+    // Extra for the task ID to be received after rotation
+    val INSTANCE_TASK_ID = "instanceTaskId"
+    // Constants for priority
+    val PRIORITY_HIGH = 1
+    val PRIORITY_MEDIUM = 2
+    val PRIORITY_LOW = 3
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_detail)
